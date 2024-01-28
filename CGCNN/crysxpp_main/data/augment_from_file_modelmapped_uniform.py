fc48fe280d1f266b567131a095bedafc574e6ab7
Ashish_Rout <45293186+Ashishrout1999@users.noreply.github.com>
2024-01-29 00:12:46 +0530

    initial commit

---
+++ CGCNN/crysxpp_main/data/augment_from_file_modelmapped_uniform.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  3 12:47:30 2022
+
+@author: ashro
+"""
+
+import os
+import shutil
+from pymatgen.ext.matproj import MPRester
+import csv
+import pandas as pd
+import numpy as np
+import multiprocessing as mp
+from tqdm import tqdm
+import random
+import pickle as pkl
+from sklearn.mixture import GaussianMixture
+from pymatgen.io.cif import CifParser
+from pymatgen.core import Structure
+from predict_energy_batch import predict_energy
+
+np.random.seed(19)
+random.seed(19)
+
+
+
+def write_structs(low, high, scale, num_per, input_dir, output_dir, modelpath):
+    """Write original and perturbed structure to a directory
+    Parameters
+    ----------
+    results: List or np.ndarray
+      query results from materials project
+    dir_name: String
+      Name of directory to write cif files to
+    Returns
+    -------
+    E: List
+      list in format to write id_prop.csv file
+    """
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    cif_files = [x for x in os.listdir(input_dir) if x[-4:] == '.cif']
+
+    for file in cif_files:
+        shutil.copy(os.path.join(input_dir, file), os.path.join(output_dir, file))
+        
+    if os.path.exists('./temp_data/'):
+        shutil.rmtree('./temp_data/')
+    
+    os.makedirs('./temp_data/')
+
+    shutil.copy('./matbench_allcifs_unperturbed_testremoved/atom_init.json', './temp_data/atom_init.json')
+    shutil.copy('./temp_data/atom_init.json', os.path.join(output_dir, 'atom_init.json'))
+
+    perturbed_cif_files = [x for x in cif_files if 'per' in x]
+    print(len(cif_files))
+    id_prop = pd.read_csv(os.path.join(input_dir, 'id_prop.csv'), header = None)
+    id_prop.columns = ['struct_idx', 'form_energy']
+    
+    original_mask = []
+    for idx in id_prop['struct_idx']:
+        if 'per' in idx:
+            original_mask.append(False)
+        else:
+            original_mask.append(True)
+
+    id_prop_original = id_prop[original_mask]
+
+    for file in perturbed_cif_files:
+        shutil.copy(os.path.join(input_dir, file), os.path.join('./temp_data/', file))
+
+    df_perturbed = predict_energy(modelpath, './temp_data/')
+
+    #print(np.mean(df.iloc[:, 1]))
+    complete_df = pd.concat([id_prop_original, df_perturbed], axis = 0, ignore_index = True)
+    print(complete_df.shape)
+    complete_df.to_csv(os.path.join(output_dir, 'id_prop.csv'), header = False, index = False)
+    
+
+
+
+
+if __name__ == "__main__":
+    low = 0.0
+    high = 0.3
+    scale_per = 1 #Scaling the perturbation distance
+    input_dir = 'matbench_allcifs_unperturbed_testremoved'
+    num_per = 1   
+    intermediate_dir = input_dir + '_per{}'.format(num_per) + '_scale{}'.format(scale_per) + '_uniform_low{}_high{}'.format(low, high)
+    output_dir = intermediate_dir + 'modelmapped' + 'bynopretrainingoptimizedcgcnnM1withadam'
+    modelpath = '../model/model_best_nopretrainingtrain132krelaxedtrain0.7val0.15test0.15epochs1000lr0.02lrmilestone800optimadamnconv4trial3.pth.tar'
+    E = write_structs(low, high, scale_per, num_per, intermediate_dir, output_dir, modelpath)
+    
---
+++ CGCNN/crysxpp_main/data/predict_energy_batch.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 22 12:52:37 2022
+
+@author: ashro
+"""
+import argparse
+import os
+import shutil
+import sys
+import time
+from tqdm import tqdm
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from sklearn import metrics
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+
+from src.data_prop import CIFData, collate_pool
+from src.model import CrystalGraphConvNet
+
+class Normalizer(object):
+    """Normalize a Tensor and restore it later. """
+    def __init__(self, tensor):
+        """tensor is taken as a sample to calculate the mean and std"""
+        self.mean = torch.mean(tensor)
+        self.std = torch.std(tensor)
+
+    def norm(self, tensor):
+        return (tensor - self.mean) / self.std
+
+    def denorm(self, normed_tensor):
+        return normed_tensor * self.std + self.mean
+
+    def state_dict(self):
+        return {'mean': self.mean,
+                'std': self.std}
+
+    def load_state_dict(self, state_dict):
+        self.mean = state_dict['mean']
+        self.std = state_dict['std']
+
+def string_to_float(x):
+    try:
+        return float(x.split('[')[1].split(']')[0])
+    except:
+        return float(x)
+    
+    
+def predict_energy(modelpath, cifdir):
+    #We can either pass structure or the cif file as input along with model.
+    #Output must be a single number with energy value.
+    
+    #Creating fake id_prop.csv to bypass error from cgcnn.
+    cif_files = [x.split('.')[0] for x in os.listdir(cifdir) if x.endswith('.cif')]
+    id_prop_df = pd.DataFrame({'cifs' : cif_files, 'energy' : [0] * len(cif_files)})
+    id_prop_df.to_csv(os.path.join(cifdir, 'id_prop.csv'), header = False, index = False)
+    
+    cuda = torch.cuda.is_available()
+    
+    model_checkpoint = torch.load(modelpath, map_location=lambda storage, loc: storage)
+    model_args = argparse.Namespace(**model_checkpoint['args'])
+    
+    dataset = CIFData(cifdir)
+    collate_fn = collate_pool
+    test_loader = DataLoader(dataset, batch_size=128, shuffle=False,
+                             num_workers=1, collate_fn=collate_fn,
+                             pin_memory=cuda)
+    
+    structures, _, _ = dataset[0]
+    orig_atom_fea_len = structures[0].shape[-1]
+    nbr_fea_len = structures[1].shape[-1]
+    model = CrystalGraphConvNet(orig_atom_fea_len, nbr_fea_len,
+                                atom_fea_len=model_args.atom_fea_len,
+                                n_conv=model_args.n_conv,
+                                h_fea_len=model_args.h_fea_len,
+                                n_h=model_args.n_h,
+                                classification=False)
+    
+    if cuda:
+        model.cuda()
+        
+    normalizer = Normalizer(torch.zeros(3))
+    model.load_state_dict(model_checkpoint['state_dict'])
+    normalizer.load_state_dict(model_checkpoint['normalizer'])
+    #print("=> loaded model '{}' (epoch {}, validation {})".format(modelpath, model_checkpoint['epoch'], model_checkpoint['best_mae_error']))
+    
+    
+    model.eval()
+    all_cif_ids = []
+    all_preds = []
+    for i, (input, target, batch_cif_ids) in enumerate(test_loader):
+        print('Batch : {}'.format(i))
+        batch_start_time = time.time()
+        with torch.no_grad():
+            if cuda:
+                input_var = (Variable(input[0].cuda(non_blocking=True)),
+                             Variable(input[1].cuda(non_blocking=True)),
+                             input[2].cuda(non_blocking=True),
+                             [crys_idx.cuda(non_blocking=True) for crys_idx in input[3]])
+            else:
+                input_var = (Variable(input[0]),
+                             Variable(input[1]),
+                             input[2],
+                             input[3])
+                
+        output, _ = model(*input_var)
+        final_output = normalizer.denorm(output.data.cpu())
+        batch_end_time = time.time()
+        print('Per batch inference time (Batch Size = 128) : {}s'.format(round(batch_end_time - batch_start_time, 2)))
+        #print(len(list(batch_cif_ids)), len(list(final_output)))
+        
+        
+        all_cif_ids.extend(list(batch_cif_ids))
+        
+        final_output_float = []
+        for i in range(len(list(final_output))):
+            final_output_float.append(string_to_float(list(final_output)[i]))
+        all_preds.extend(final_output_float)
+    
+    
+    df = pd.DataFrame()
+    df['struct_idx'] = all_cif_ids
+    df['form_energy'] = all_preds
+    return df
+
+if __name__=='__main__':
+    modelpath = './model_best_trainmpdata75-230_testmpdata75-230.pth.tar'
+    cifdir = '../Data/One_Item/'
+    predict_energy(modelpath, cifdir)
